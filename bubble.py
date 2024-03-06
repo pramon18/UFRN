8b45e2a420feaa1225f2fbed536114207a4986ff
Pablo Ramon Varela de Souza <pabloramon044@gmail.com>
2024-03-05 21:27:58 -0300

    Ajuste no algoritmo

--- bubble.py
+++ bubble.py
@@ -2,13 +2,14 @@
 lista = [5,3,2,1,5,7,9]
 
 for i in range(len(lista)):
-  for j in range(len(lista)):
+  for j in range(len(lista)-1):
     print(lista[j])
-    print(lista[i] > lista[j])
-    if(lista[i] < lista[j]):
+    print(lista[j + 1])
+    print(lista[j] > lista[j + 1])
+    if(lista[j] > lista[j + 1]):
         aux = lista[j]
-        lista[j] = lista[i]
-        lista[i] = aux
+        lista[j] = lista[j + 1]
+        lista[j + 1] = aux
       
   print("---------------")  
 print(lista)
